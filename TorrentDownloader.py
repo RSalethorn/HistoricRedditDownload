ed6ac001ea12329c3e621632e1c7c4013fe44b0f
Rob Salethorn <rob.salethorn@placeholder.com>
2023-07-11 18:51:27 +0100

    Downloaded files now save to folder in project & script waits for torrent to be added

--- TorrentDownloader.py
+++ TorrentDownloader.py
@@ -3,11 +3,19 @@ from datetime import datetime
 import time
 #Secret Config File
 import config
+import os
 
 # Declare magnet_link for Torrent of PushshiftDump
 magnet_link = "magnet:?xt=urn:btih:7c0645c94321311bb05bd879ddee4d0eba08aaee&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"
 # Extract hash from magnet link
 torrent_hash = magnet_link.split("&")[0].split(":")[-1]
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+print(current_directory)
+download_location = current_directory + "\\Download\\"
+saved_location = current_directory + "\\Saved\\"
+print(download_location)
+print(saved_location)
 
 # Get QBitTorrent Connection Details
 conn_info = config.conn_info
@@ -26,14 +34,23 @@ except qbittorrentapi.LoginFailed as e:
     print(e)
 
 # Add torrent to QBT
-qb.torrents_add(urls=magnet_link)
+qb.torrents_add(urls=magnet_link, save_path=saved_location, download_path=download_location)
 
 # Sync torrents? (May not be needed unsure)
 qb.torrents_info()
 
-# Check through all torrents in client for torrent that shares the hash of magnet link
-matching_torrents = [torrent for torrent in qb.torrents_info() if torrent.hash == torrent_hash]
-torrent = matching_torrents[0]
+# Wait loop incase API running slow and torrent hasn't been added yet
+torrent_added = False
+while torrent_added == False:
+    # Check through all torrents in client for torrent that shares the hash of magnet link
+    matching_torrents = [torrent for torrent in qb.torrents_info() if torrent.hash == torrent_hash]
+    try:
+        torrent = matching_torrents[0]
+        # torrent_added only changed if previous line doesn't cause error. (Needs verifying)
+        torrent_added = True
+    except IndexError:
+        print("Waiting for torrent being added")
+        time.sleep(1)
 
 # Loop gets program to wait for metadata of torrent to be downloaded
 metadata_downloaded = False
@@ -45,6 +62,7 @@ while metadata_downloaded == False:
         metadata_downloaded = True
     time.sleep(1)
 
+# Loop through all files in Pushshift torrent
 for file in torrent.files:
     # Convert file name into date
     date_str = file.name.split("_")[-1].split(".")[0] + "-01"
@@ -55,7 +73,7 @@ for file in torrent.files:
         print(file.name)
         metadata_downloaded = True
     else:
-        # If not don't download
+        # If not don't download (Priority = 0)
         qb.torrents_file_priority(torrent_hash, file['id'], 0)
         
         
